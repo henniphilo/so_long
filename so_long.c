880a9a3f81bab1349ca2b114e73c236ce00c3de5
Henrike Wiemann <hwiemann@c4b9c5.42berlin.de>
2023-07-31 16:24:35 +0200

    monday

--- so_long.c
+++ so_long.c
@@ -1,13 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hwiemann <hwiemann@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/31 11:32:51 by hwiemann          #+#    #+#             */
+/*   Updated: 2023/07/31 16:17:23 by hwiemann         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <mlx.h>
 
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <X11/X.h>
+#include <X11/keysym.h>
+#include <mlx.h>
+
+#define WINDOW_WIDTH 600
+#define WINDOW_HEIGHT 300
+
+#define MLX_ERROR 1
+
+#define RED_PIXEL 0xFF0000
+
 typedef struct	s_data {
+	void	*
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
 	int		line_length;
 	int		endian;
 }				t_data;
-
+/*
+ typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+}				t_vars;
+*/
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
@@ -16,26 +48,25 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-
- typedef struct	s_vars {
-	void	*mlx;
-	void	*win;
-}				t_vars;
-
-int	close(int keycode, t_vars *vars)
+int	close(int keycode, t_vars *vars) //need to use keycode esc
 {
-	mlx_destroy_window(vars->mlx, vars->win);
+	mlx_destroy_window(data->mlx, vars->win);
 	return (0);
 }
+
+
+// mlx_hook(vars.win, ON_DESTROY, 0, close, &vars);
 
 int	main(void)
 {
 	t_data	img;
-	t_vars	vars;
+	void	*mlx;
+	void	*mlx_win;
+//	t_vars	vars;
 
-	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 1920, 1080, "Sooooo Long");
-	img.img = mlx_new_image(vars.mlx, 1920, 1080);
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Sooooo Long");
+	img.img = mlx_new_image(mlx, 1920, 1080);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 								&img.endian);
 	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
@@ -48,10 +79,9 @@ int	main(void)
 	my_mlx_pixel_put(&img, 5, 25, 0x00FF0000);
 
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_hook(vars.win, 2, 1L<<0, close, &vars);
-	mlx_loop(vars.mlx);
+//	mlx_hook(vars.win, 2, 1L<<0, close, &vars.win);
+	mlx_loop(mlx);
 }
-// ein roter dot mit ueberschrift so long
 /*
 int	main(void)
 {
